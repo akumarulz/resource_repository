b17a1a00a671507d527566e9f1cd766a8c24b55c
akumarulz <darday1984@gmail.com>
2017-04-18 01:29:44 +0100

    commit

--- delete_message.php
+++ delete_message.php
@@ -17,24 +17,5 @@ if(strtoupper($_SERVER['REQUEST_METHOD']) === 'GET') {
 		echo '-1';
 	}
 }
-=======
-<?php
-if(strtoupper($_SERVER['REQUEST_METHOD']) === 'GET') {
-	require_once('connect.php');
-	require_once('autoload.php');
-//	$user_id = $_GET['user_id'];
-	$inboxitem = $_GET['inboxitem'];
 
-	$var = new database_query($pdo,'messages');
-	$remove = new inboxitemobj();
-	$remove->setMessageid($inboxitem);
-	$remove->setConn($var);
-	$result = $remove->removeMessage();
-	if($result){
-		echo '1';
-	}else{
-		echo '-1';
-	}
-}
->>>>>>> origin/master
 ?>
